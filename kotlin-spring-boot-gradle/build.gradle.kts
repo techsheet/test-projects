4c8be253aad26f7a600ee722e368a63ff876676c
frne <fneff89@gmail.com>
2026-05-04 14:35:57 +0200

    Actually use ktlint, not only expect it

--- kotlin-spring-boot-gradle/build.gradle.kts
+++ kotlin-spring-boot-gradle/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	alias(libs.plugins.kotlinJvm)
 	alias(libs.plugins.kotlinSpring)
+	alias(libs.plugins.ktlint)
 	alias(libs.plugins.springBoot)
 	alias(libs.plugins.springDependencyManagement)
 }
